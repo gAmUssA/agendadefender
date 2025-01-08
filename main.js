772689c19f870b87152121d3d61cf3ed189e399d
Viktor Gamov <gamussa@gmail.com>
2025-01-08 14:54:24 -0500

    added electron build of the app

---
+++ main.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    titleBarStyle: 'hidden',
+    trafficLightPosition: { x: 20, y: 20 }
+  });
+
+  // Load the local HTML file
+  win.loadFile('index.html');
+  
+  // Open DevTools in development
+  // win.webContents.openDevTools();
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
